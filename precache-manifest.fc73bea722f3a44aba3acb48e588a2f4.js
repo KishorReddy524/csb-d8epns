f945bd580ae02ee5062f8208858fb03545f76230
Code Sandbox <hello@codesandbox.io>
2022-04-30 10:19:22 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.fc73bea722f3a44aba3acb48e588a2f4.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48747cdde3ca48ca07e922a04b75e2ff",
+    "url": "https://kishorreddy524.github.io/csb-d8epns/index.html"
+  },
+  {
+    "revision": "47635fdcfe90d4ad73cd",
+    "url": "https://kishorreddy524.github.io/csb-d8epns/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "2ce24b195c9ea27f8170",
+    "url": "https://kishorreddy524.github.io/csb-d8epns/static/css/main.6db8c076.chunk.css"
+  },
+  {
+    "revision": "f4da1b594a826692e6da",
+    "url": "https://kishorreddy524.github.io/csb-d8epns/static/js/2.d180ea3f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "https://kishorreddy524.github.io/csb-d8epns/static/js/2.d180ea3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47635fdcfe90d4ad73cd",
+    "url": "https://kishorreddy524.github.io/csb-d8epns/static/js/3.15144f2a.chunk.js"
+  },
+  {
+    "revision": "2ce24b195c9ea27f8170",
+    "url": "https://kishorreddy524.github.io/csb-d8epns/static/js/main.731b39bb.chunk.js"
+  },
+  {
+    "revision": "36102bf840ef48ad8fee",
+    "url": "https://kishorreddy524.github.io/csb-d8epns/static/js/runtime-main.5bac8798.js"
+  },
+  {
+    "revision": "29bfc020b1bfc934238f23afb43c6561",
+    "url": "https://kishorreddy524.github.io/csb-d8epns/static/media/AMCCOLLEGELOGO.29bfc020.png"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "https://kishorreddy524.github.io/csb-d8epns/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "07a898a10184fea3be4de3c85d94ab3b",
+    "url": "https://kishorreddy524.github.io/csb-d8epns/static/media/CIRCLE1.07a898a1.png"
+  },
+  {
+    "revision": "07a898a10184fea3be4de3c85d94ab3b",
+    "url": "https://kishorreddy524.github.io/csb-d8epns/static/media/CIRCLE2.07a898a1.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "https://kishorreddy524.github.io/csb-d8epns/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "8134f62b6df1886041da14eab34705e5",
+    "url": "https://kishorreddy524.github.io/csb-d8epns/static/media/bestBuy.comLogo.8134f62b.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "https://kishorreddy524.github.io/csb-d8epns/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "https://kishorreddy524.github.io/csb-d8epns/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "358f8f1fdef01d3c0a8306f8f80973c2",
+    "url": "https://kishorreddy524.github.io/csb-d8epns/static/media/masai.358f8f1f.358f8f1f.jpeg"
+  },
+  {
+    "revision": "db2a0b0b20bb01af79b9a7379eae40f3",
+    "url": "https://kishorreddy524.github.io/csb-d8epns/static/media/python.db2a0b0b.jpg"
+  },
+  {
+    "revision": "dc728b861c4f51b2bc5d7985c32af5d6",
+    "url": "https://kishorreddy524.github.io/csb-d8epns/static/media/upGradeLogo.dc728b86.png"
+  }
+]);
